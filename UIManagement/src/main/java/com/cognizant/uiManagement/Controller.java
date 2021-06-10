f093c0bebffada4d56bcfd06fc2f654cac4849e3
nithish2220 <56596182+nithish2220@users.noreply.github.com>
2021-06-10 09:38:15 +0530

    update Controller.java
    Updated @RestController to @Controller

--- UIManagement/src/main/java/com/cognizant/uiManagement/Controller.java
+++ UIManagement/src/main/java/com/cognizant/uiManagement/Controller.java
@@ -1,79 +1,70 @@
 package com.cognizant.uiManagement;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import io.swagger.annotations.ApiModel;
 
-@RestController
+@org.springframework.stereotype.Controller
 @ApiModel("Account Management Module")
 public class Controller {
 	
-	public ModelAndView Default(String jspPage) {
-		ModelAndView mav= new ModelAndView();
-		mav.setViewName(jspPage);
-		return mav;
-	}
 	
 	//Employee Index Page 
 	@GetMapping("/emp")
-	public ModelAndView bankemphome() {
-		return Default("empindex");
+	public String bankemphome() {
+		return "empindex";
 	}
 	
 	//Create New Employee 
 	@GetMapping("/create-emp-acc")
-	public ModelAndView newBankemp() {
-		return Default("newemp");
+	public String newBankemp() {
+		return "newemp";
 	}
 	
 	//Employee Creation Success
 	@GetMapping("/esuccess")
-	public ModelAndView empSuccess() {
-		return Default("ecreationsuccess");
+	public String empSuccess() {
+		return "ecreationsuccess";
 	}
 	
 	//Employee Login
 	@GetMapping("/elogin")
-	public ModelAndView login() {
-		return Default("Elogin");
+	public String login() {
+		return "Elogin";
 	}
 	
 	//Employee Logged inside 
 	@GetMapping("/inside")
-	public ModelAndView loginSuccess() {
-		return Default("EBankMe");
+	public String loginSuccess() {
+		return "EBankMe";
 	}
 	
 	//Employee Creating new Account for Customer
 	@GetMapping("/emp-create-new-cust")
-	public ModelAndView empNewCust() {
-		return Default("empNewCustomer");
+	public String empNewCust() {
+		return "empNewCustomer";
 	}
 	
 	//Employee Getting all the details regarding Loans
 	@GetMapping("/emp-get-all-loans")
-	public ModelAndView getAllLoans() {
-		return Default("empGetLoans");
+	public String getAllLoans() {
+		return "empGetLoans";
 	}
 	
 	//Employee Getting all Customer Details
 	@GetMapping("/emp-get-all-cust")
-	public ModelAndView getAllCust() {
-		return Default("empGetAllCustomers");
+	public String getAllCust() {
+		return "empGetAllCustomers";
 	}
 	
 	@GetMapping("/cust")
-	public ModelAndView custhome() {
-		return Default("custindex");
+	public String custhome() {
+		return "custindex";
 	}
 
 	@GetMapping("/custlogin")
-	public ModelAndView custlogin() {
-		return Default("custlogin");
+	public String custlogin() {
+		return "custlogin";
 	}
 	
 }
