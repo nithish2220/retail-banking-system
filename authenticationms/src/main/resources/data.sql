3a57a84d58a718e43b1ad3ab87b1ed8e1c880408
nithish2220 <56596182+nithish2220@users.noreply.github.com>
2021-06-17 12:11:21 +0530

    Update data.sql

--- authenticationms/src/main/resources/data.sql
+++ authenticationms/src/main/resources/data.sql
@@ -1,2 +1,3 @@
 insert into appuser (userid,username,password,role) values ('EMPLOYEE101','emp','emp','EMPLOYEE');
-insert into appuser (userid,username,password,role) values ('CUSTOMER101','cust','cust','CUSTOMER');
+insert into appuser (userid,username,password,role) values ('CUSTOMER101','Pulkit sav','cust','CUSTOMER');
+insert into appuser (userid,username,password,role) values ('CUSTOMER102','Pulkit cur','customer','CUSTOMER');
