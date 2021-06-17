92480a1b71cce67e30e03629bbd0ab73dfdf2000
nithish2220 <56596182+nithish2220@users.noreply.github.com>
2021-06-17 12:10:50 +0530

    Update data.sql

--- accountms/accountms/src/main/resources/data.sql
+++ accountms/accountms/src/main/resources/data.sql
@@ -1,4 +1,5 @@
+INSERT INTO ACCOUNT(account_Id,customer_Id,current_Balance,account_Type,opening_date,owner_Name)
+VALUES (1000000001,'CUSTOMER101',1000.00, 'Savings',TO_DATE('09/10/2021', 'DD/MM/YYYY'), 'Pulkit sav');
 
-
-INSERT INTO ACCOUNT values (123456,'Savings', 1000.00, 'CUSTOMER103', '2021/14/06', 'Pulkit sav');
-
+INSERT INTO ACCOUNT(account_Id,customer_Id,current_Balance,account_Type,opening_date,owner_Name)
+VALUES (1000000002, 'CUSTOMER102', 2000.00, 'Current', TO_DATE('09/10/2021', 'DD/MM/YYYY'),'Pulkit cur');
