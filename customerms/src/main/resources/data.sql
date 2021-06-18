225723badb23465e1a4d9f2c4413590a762b3e8b
nithish2220 <56596182+nithish2220@users.noreply.github.com>
2021-06-18 17:49:19 +0530

    Delete customerms directory

--- customerms/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-insert into CUSTOMER_ENTITY values('CUSTOMER101','Chennai','1999-02-02','ABCDE1234A','cust','Pulkit sav')
-insert into CUSTOMER_ENTITY values('CUSTOMER102','Chennai','1999-02-02','ABCDE1234C','customer','Pulkit cur')
