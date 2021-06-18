689661809add3a0f8423b916b73ff9e62db8d3e7
nithish2220 <56596182+nithish2220@users.noreply.github.com>
2021-06-18 20:47:52 +0530

    Update AppUser.java

--- customerms/src/main/java/com/cognizant/customerservice/model/AppUser.java
+++ customerms/src/main/java/com/cognizant/customerservice/model/AppUser.java
@@ -19,9 +19,11 @@ import lombok.Setter;
 public class AppUser {
 	
 	@Id
+	@Pattern(regexp = "^[A-Za-z0-9]*$")
 	@Column(name = "userid", length = 20)
 	private String userid;
 	
+	@Pattern(regexp = "^[A-Za-z0-9]*$")
 	@Column(name = "username", length = 20)
 	private String username;
 	
