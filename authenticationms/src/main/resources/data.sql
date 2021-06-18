851c3e3158b6c073fd47361e07148d8e6c1769ba
nithish2220 <56596182+nithish2220@users.noreply.github.com>
2021-06-18 17:49:00 +0530

    Delete authenticationms directory

--- authenticationms/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into appuser (userid,username,password,role) values ('EMPLOYEE101','emp','emp','EMPLOYEE');
-insert into appuser (userid,username,password,role) values ('CUSTOMER101','Pulkit sav','cust','CUSTOMER');
-insert into appuser (userid,username,password,role) values ('CUSTOMER102','Pulkit cur','customer','CUSTOMER');
